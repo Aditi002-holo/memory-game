ea555b9a7347c388260acdb411fab2ee6d0631de
Aditi002-holo <aditideshpande2908@gmail.com>
2022-09-01 20:26:59 +0530

    add comments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// create a memory cards 'array of objects'
 const cardArray = [
     {
         name: 'fries',
@@ -49,46 +50,62 @@ const cardArray = [
     }
 ]
 
+// get random order of array each time >> little trick used here
 cardArray.sort(() => 0.5 - Math.random());
 
+// select the elements  - to be impacted -
 const gridDisplay = document.querySelector('#grid');
 const resultDisplay = document.querySelector('#result');
+
+// initialize variables required to choose cards
 let cardsChosen = []
 let cardsChosenIds = []
 const cardsWon = []
 
+// creating a card 
 function createBoard() {
     for(let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img');
         card.setAttribute('src','images/blank.png');
         card.setAttribute('data-id', i);
-        // console.log(card, i);
 
+        // listen to the card clicked and call flipCard()
         card.addEventListener('click', flipCard);
 
+        // append the card under grid element
         gridDisplay.appendChild(card);
     }
 }
 
 createBoard();
 
+// to check if it's a match
 function checkMatch() {
-    const cards = document.querySelectorAll('#grid img')
+    const cards = document.querySelectorAll('#grid img') // grid >> card (child)
+
+    
     if(cardsChosen[0] === cardsChosen[1]) {
         alert('you found a match');
+
+        // set background white if it's a match
         cards[cardsChosenIds[0]].setAttribute('src','images/white.png');
         cards[cardsChosenIds[1]].setAttribute('src','images/white.png');
-         
+        
+        // remove click event
         cards[cardsChosenIds[0]].removeEventListener('click',flipCard);
         cards[cardsChosenIds[1]].removeEventListener('click',flipCard);
 
         cardsWon.push(cardsChosen);
     } else {
+        // flip the card back to original if not a match
         cards[cardsChosenIds[0]].setAttribute('src','images/blank.png');
         cards[cardsChosenIds[1]].setAttribute('src','images/blank.png');
         alert('sorry try again')
     }
+    // display the score
     resultDisplay.innerText = cardsWon.length;
+
+    // empty the arrays - for the next play
     cardsChosen = [];
     cardsChosenIds = [];
 
@@ -99,12 +116,15 @@ function checkMatch() {
 }
 
 function flipCard() {
+    // get card name and id to display after flipping (click & flip)
     const cardId = this.getAttribute('data-id');
     cardsChosen.push(cardArray[cardId].name);
     cardsChosenIds.push(cardId);
 
+    // set the image 
     this.setAttribute('src', cardArray[cardId].img);
 
+    // check for a match after every 2 cards are flipped
     if(cardsChosen.length === 2) {
         setTimeout(checkMatch, 500);
     }
